4b7e8ac2f47f162d3b04c903cf00ff0a261f3c09
Michael O'Sullivan <michael@nizamimedia.com>
2019-08-08 23:57:43 +0100

    more features to the popup

--- reward-modal.php
+++ reward-modal.php
@@ -3,35 +3,37 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Reward Creator and Publishers</h5>
+        <h5 class="modal-title" id="exampleModalCenterTitle">SAFE SECURE INSTANT REWARDS <i class="fas fa-shield-alt"></i></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body text-left">
-      <p class="lead">This month you have already earned <img src="images/wirebit-icon.png"> <strong>1542.4523 WIRE</strong> for just reading the news with no ads! Please Reward generously to the Creator:</p>
+      <p class="lead">This month you have earned <img src="images/wirebit-icon.png"> <strong>1542.4523 WIRE</strong> for just reading the news with no ads! Please Reward generously to the Creator and help keep our platform operational:</p>
       <br />
 
             <p class="mt-1"><button class="btn btn-warning mb-1"><img src="images/wirebit-icon.png"> 1.00 WIRE</button> <span class="grey-text">This is roughly £0.70</span></p>
             <p class="mt-1"><button class="btn btn-warning mb-1"><img src="images/wirebit-icon.png"> 5.00 WIRE</button> <span class="grey-text">This is roughly £2.50</span></p>
             <p class="mt-1"><button class="btn btn-warning mb-1"><img src="images/wirebit-icon.png"> 10.00 WIRE</button> <span class="grey-text">This is roughly £7.00</span></p>
-            <p class="mt-1"><button class="btn btn-warning mb-1"><img src="images/wirebit-icon.png"> 50.00 WIRE</button> <span class="grey-text">This is roughly £25.00</span></p>
+            <p class="mt-1"><button class="btn btn-warning mb-1 active"><img src="images/wirebit-icon.png"> 50.00 WIRE</button> <span class="grey-text">This is roughly £25.00</span></p>
 
 
             <form>
-  <div class="form-group row">
-    <label for="staticEmail" class="col-sm-5 col-form-label">Choose your own</label>
-    <div class="col-sm-7">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" placeholder="24.26">
-    </div>
+            <div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text"><img src="images/wirebit-icon.png"></span>
+    <span class="input-group-text">50.00</span>
   </div>
+  <input type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)">
+</div>
 
-  <p class="mt-1"><button class="btn btn-warning mb-1"><img src="images/wirebit-icon.png"> WIRE</button> This is roughly £</p>
-
-
+  
+  <p></p>
+  <p class="mt-1"><button type="button" class="btn btn-success btn-lg btn-block" data-dismiss="modal">SEND <img src="images/wirebit-icon.png"> 50.00</button></p>
+  <p class="mt-1">Remaining balance after sending Reward: <img src="images/wirebit-icon.png"> 1492.4523<strong></strong></p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-bywire" data-dismiss="modal">Top Up</button>
+        <button type="button" class="btn btn-bywire" data-dismiss="modal">Need more WIRE? Top Up</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
       </div>
     </div>
