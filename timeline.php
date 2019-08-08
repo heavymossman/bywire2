dfcc8b2e7ab1f6d23a5d4432e792c63be85fcc14
Michael O'Sullivan <michael@nizamimedia.com>
2019-08-08 01:04:29 +0100

    Complete body of the timeline with mulple examples of how postings should look with varied images available and video content

--- timeline.php
+++ timeline.php
@@ -1,21 +1,540 @@
-
 <?php include 'header.php';?>
-
 <?php include 'nav.php';?>
+<?php include 'logo-header.php';?>
+<div class="container mt-5 ">
+	<!--MASTER CONTAINER-->
+	<div class="row">
+		<!--MASTER ROW-->
+		<div class="col-md-9">
+			<!--MASTER COL-MD--9 DESKTOP-->
+			<section class="mb-5">
+				<div class="row light-grey-bk hidden-mobile ">
 
-    <?php include 'logo-header.php';?>
+                    <div class="col-6">
+                        <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+                    </div>
+
+
+                    <div class="col-6 text-right">
+                        <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+                    </div>
+
+                </div>
+
+                <div class="row p-1 light-grey-bk hidden-mobile">
+               
+                
+                    <div class="col-1 p-1">
+                        <img src="images/profile-logo.png" class="img-fluid" >
+                    </div>
+
+                    <div class="col-2 p-1">
+                        <p class="mt-5"><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+                    </div>
+
+                    <div class="col-5 p-1"></div>
+			
+				
+					<div class="col-4 p-1 text-right">
+						<p class="mt-3"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+					</div>
+                </div>
+                
+
+                <div class="row light-grey-bk hidden-desktop">
+
+                    <div class="col-6">
+                        <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+                    </div>
+
+
+                    <div class="col-6 text-right">
+                        <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+                    </div>
+
+                </div>
+
+                <div class="row p-2 light-grey-bk hidden-desktop">
+               
+                
+                    <div class="col-3 p-1">
+                        <img src="images/profile-logo.png" class="img-fluid" >
+                    </div>
+
+                    <div class="col-1"></div>
+
+                    <div class="col-4 ">
+                        <p><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+						<p class="mt-2"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+                    </div>
+                </div>
+                
+				
+				<div class="row medium-grey-bk">
+					<div class="col-md-7 p-3">
+						<div class="row my-2">
+							<div class="col-12">
+								<img src="images/time-line-image1.jpg" class="img-fluid"></a>
+							</div>
+						</div>
+						<div class="row mt-4">
+							<div class="col-6">
+								<img src="images/time-line-image2.jpg" class="img-fluid"></a>
+							</div>
+							<div class="col-6">
+								<img src="images/time-line-image3.jpg" class="img-fluid"></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-12 mt-2">
+								<ul class="nav socialcons share-icons  pb-2 text-center">
+									<li class="nav-item">
+										<a class="nav-link active " href="#"><i class="far fa-smile-beam "></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-surprise"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-meh-rolling-eyes"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-sad-tear"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-angry"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<p><small><a href="#" class="red-text">#Politics</a> <a href="#" class="red-text">#Johnson</a> <a href="#" class="red-text">#Brexit</a></small></p>
+						<h2 class="pb-3"><a href="#">'Deep state PLOT to kill Brexit' Johnson delivers CRUCIAL reason why exit can't be halted</a></h2>
+						<p>WASHINGTON — Since the start of the Trump administration, Britain has helped lead the quiet resistance to a president upending American foreign policy and straining the trans-Atlantic alliance.</p>
+						<p>That may change with Boris Johnson’s ascent to the post of prime minister of Britain on Wednesday. With his showmanship, his fondness for broad declarations and his transactional politics, Mr. Johnson, or “BoJo” as he is commonly known, is cut from Trumpian.</p>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-mobile">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-desktop">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <br/><i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <br/><i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <br/><i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+            </section>
+            
+
+            <section class="mb-5">
+            <div class="row light-grey-bk hidden-mobile">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-1 light-grey-bk hidden-mobile">
+
+
+<div class="col-1 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-2 p-1">
+    <p class="mt-5"><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+</div>
+
+<div class="col-5 p-1"></div>
+
+
+<div class="col-4 p-1 text-right">
+    <p class="mt-3"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+
+
+<div class="row light-grey-bk hidden-desktop">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-2 light-grey-bk hidden-desktop">
+
+
+<div class="col-3 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-1"></div>
+
+<div class="col-4 ">
+    <p><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+    <p class="mt-2"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+				<div class="row medium-grey-bk">
+					<div class="col-md-7 p-3">
+						<div class="row my-2">
+							<div class="col-12">
+								<img src="images/time-line-image5.jpg" class="img-fluid"></a>
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-12 mt-2">
+								<ul class="nav socialcons share-icons  pb-2 text-center">
+									<li class="nav-item">
+										<a class="nav-link active " href="#"><i class="far fa-smile-beam "></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-surprise"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-meh-rolling-eyes"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-sad-tear"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-angry"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<p><small><a href="#" class="red-text">#Politics</a> <a href="#" class="red-text">#Johnson</a> <a href="#" class="red-text">#Brexit</a></small></p>
+						<h2 class="pb-3"><a href="#">'Britain will manage risks of Brexit - Foreign Secretary Raab</a></h2>
+						<p>Britain will manage the risks of its withdrawal from the European Union "come what may," and is determined to forge ahead with Brexit, its Foreign Secretary Dominic Raab said on Wednesday after talks with his U.S. counterpart in Washington.</p>
+						<p>"We will manage the risks come what may. We will leave at the end of October and are determined to make a success of it," he told a joint news conference with U.S. Secretary of State Mike Pompeo.</p>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-mobile">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-desktop">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <br/><i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <br/><i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <br/><i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+            </section>
+            
+
+            <section class="mb-5">
+            <div class="row light-grey-bk hidden-mobile">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-1 light-grey-bk hidden-mobile">
+
+
+<div class="col-1 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-2 p-1">
+    <p class="mt-5"><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+</div>
+
+<div class="col-5 p-1"></div>
+
+
+<div class="col-4 p-1 text-right">
+    <p class="mt-3"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+
+
+<div class="row light-grey-bk hidden-desktop">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-2 light-grey-bk hidden-desktop">
+
+
+<div class="col-3 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-1"></div>
+
+<div class="col-4 ">
+    <p><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+    <p class="mt-2"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+				<div class="row medium-grey-bk">
+					<div class="col-md-7 p-3">
+						<div class="row my-2">
+							<div class="col-12">
+								<img src="images/time-line-image6.jpg" class="img-fluid"></a>
+							</div>
+                        </div>
+                        <div class="row my-2">
+							<div class="col-12">
+								<img src="images/time-line-image7.jpg" class="img-fluid"></a>
+							</div>
+						</div>
+						
+					</div>
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-12 mt-2">
+								<ul class="nav socialcons share-icons  pb-2 text-center">
+									<li class="nav-item">
+										<a class="nav-link active " href="#"><i class="far fa-smile-beam "></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-surprise"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-meh-rolling-eyes"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-sad-tear"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-angry"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<p><small><a href="#" class="red-text">#Politics</a> <a href="#" class="red-text">#Johnson</a> <a href="#" class="red-text">#Brexit</a></small></p>
+						<h2 class="pb-3"><a href="#">Yet another mass killing in the US, what can politicans really do to make any positive changes?</a></h2>
+						<p>President Donald Trump on Monday proposed tighter monitoring of the internet, mental health reform and wider use of the death penalty in response to two mass shootings over the weekend that left 32 people dead in Texas and Ohio.</p>
+						<p>Trump, a Republican, whom Democrats have accused of stoking racial divisions, said Americans must "condemn racism, bigotry and white supremacy," a day after Texas officials said racial...</p>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-mobile">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-desktop">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <br/><i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <br/><i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <br/><i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+            </section>
+            
+
+
+
+            <section class="mb-5">
+            <div class="row light-grey-bk hidden-mobile">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-1 light-grey-bk hidden-mobile">
+
+
+<div class="col-1 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-2 p-1">
+    <p class="mt-5"><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+</div>
+
+<div class="col-5 p-1"></div>
+
+
+<div class="col-4 p-1 text-right">
+    <p class="mt-3"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+
+
+<div class="row light-grey-bk hidden-desktop">
+
+<div class="col-6">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-question-circle"></i></a></p>
+</div>
+
+
+<div class="col-6 text-right">
+    <p class="m-0 p-0"> <a href="#"><i class="far fa-bookmark"></i></a></p>
+
+</div>
+
+</div>
+
+<div class="row p-2 light-grey-bk hidden-desktop">
+
+
+<div class="col-3 p-1">
+    <img src="images/profile-logo.png" class="img-fluid" >
+</div>
+
+<div class="col-1"></div>
+
+<div class="col-4 ">
+    <p><small>By <a href="#" class="red-text">Darren Hunt</a></small></p>
+    <p class="mt-2"><button class="btn btn-bywire btn-lg"><img src="images/wb-logo.png"> Reward Creator</button></p>
+</div>
+</div>
+				<div class="row medium-grey-bk">
+					<div class="col-md-7 p-3">
+						<div class="row my-2">
+							<div class="col-12">
+								<!-- 16:9 aspect ratio -->
+                                <div class="embed-responsive embed-responsive-4by3">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                                </div>
+							</div>
+                        </div>
+                       
+						
+					</div>
+					<div class="col-md-5">
+						<div class="row">
+							<div class="col-12 mt-2">
+								<ul class="nav socialcons share-icons  pb-2 text-center">
+									<li class="nav-item">
+										<a class="nav-link active " href="#"><i class="far fa-smile-beam "></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-surprise"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-meh-rolling-eyes"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"> <i class="far fa-sad-tear"></i></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#"><i class="far fa-angry"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<p><small><a href="#" class="red-text">#Politics</a> <a href="#" class="red-text">#Johnson</a> <a href="#" class="red-text">#Brexit</a></small></p>
+						<h2 class="pb-3"><a href="#">Can Arsenal win any major competition this year or are they doomed foreve?</a></h2>
+						<p>President Donald Trump on Monday proposed tighter monitoring of the internet, mental health reform and wider use of the death penalty in response to two mass shootings over the weekend that left 32 people dead in Texas and Ohio.</p>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-mobile">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+				<div class="row pt-3 pb-2 medium-grey-bk hidden-desktop">
+					<div class="col-4">
+						<button class="btn btn-bywire btn-block">MORE <br/><i class="fas fa-chevron-right ml-2" aria-hidden="true"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-primary btn-block">SHARE <br/><i class="fas fa-share ml-2"></i></button>
+					</div>
+					<div class="col-4">
+						<button class="btn btn-info btn-block">COMMENT <br/><i class="far fa-comment ml-2"></i><strong> 64</strong></button>
+					</div>
+				</div>
+			</section>
 
 
 
 
 
-
-
-
-
-
-
-
-    		
-
-    <?php include 'footer.php';?>
+		</div>
+		<div class="col-md-3">
+			<!--MASTER COLMD-3 DESKTOP-->
+		</div>
+	</div>
+	<!--MASTER ROW -->
+</div>
+<!--MASTER CONTAINER-->
+<?php include 'footer.php';?>
