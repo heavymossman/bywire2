79a9f065666b828122515328804317d0b09604e9
Michael O'Sullivan <michael@nizamimedia.com>
2019-08-16 20:21:36 +0100

    margin top of form

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
     <hr />
 
 <form>
-  <div class="form-group">
+  <div class="form-group mt-5">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
